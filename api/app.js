f0b73478181422b16d18581338f0a7c607e72668
Evandro oliveira <vandoaparecido@hotmail.com>
2020-12-01 01:57:50 -0300

    Conectando com o banco mongoDb

--- api/app.js
+++ api/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/orcamento', function (req,res){
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/myapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then(() =>{
+    console.log("Conectado com sucesso");
+}).catch((err) =>{
+    console.log("Falha na conexão: " + err);
+});
+
+app.post('/orcamento',async (req,res) => {
+    console.log(req.body);
     res.send('hello world');
 })
 
